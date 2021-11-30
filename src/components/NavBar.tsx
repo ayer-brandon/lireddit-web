e2c6346132702dc7b919b7495a1b23c9384a46c2
Brandon Ayer <b.ayer@resourceenergysolutions.com>
2021-11-30 05:32:17 -0700

    create ui

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,58 @@
+import { Box, Flex, Link } from "@chakra-ui/layout";
+import React from "react";
+import NextLink from "next/link";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { Button } from "@chakra-ui/button";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  const [{fetching: logoutFetching}, logout] = useLogoutMutation();
+
+  let body = null;
+
+  //loading
+  if (fetching) {
+  }
+  //not logged in
+  else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link color="white" mr={2}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white">Register</Link>
+        </NextLink>
+      </>
+    );
+  }
+  //logged in
+  else {
+    body = (
+      <Flex>
+        <Box color="white" mr={2}>
+          {data.me.username}
+        </Box>
+        <Button
+          variant="link"
+          onClick={() => {
+            logout();
+          }}
+          isLoading={logoutFetching}
+        >
+          logout
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
